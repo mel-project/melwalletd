81d9b95a04ac65a831503da823ead349d057e6bd
Shane Mendez <mendezsoftware@gmail.com>
2022-07-27 17:23:54 -0500

    fixed typo

--- src/cli.rs
+++ src/cli.rs
@@ -27,7 +27,7 @@ pub struct Args {
     pub network: NetID,
 
     #[clap(long, display_order(3))]
-    /// IP of full node on specified `network`; Required when not collecting to "mainnet" or "testnet"
+    /// IP of full node on specified `network`. Required when using networks other than "mainnet" and "testnet"
     pub connect: Option<SocketAddr>,
 
     #[clap(long, default_value = "127.0.0.1:11773", display_order(4))]
